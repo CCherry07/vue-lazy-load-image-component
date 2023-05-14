dd6b9429085c78fc455dfacedef3cfd177189f15
CCherry07 <2405693142@qq.com>
2023-05-15 01:07:24 +0800

    ci: update eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,5 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: ['plugin:vue/vue3-recommended', 'prettier', 'plugin:prettier/recommended'],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  rules: {
-    'vue/no-v-html': 'off',
-    'vue/require-explicit-emits': 'off',
-    'vue/require-prop-types': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/no-reserved-keys': 'off',
-    'vue/comment-directive': 'off',
-    'vue/prop-name-casing': 'off',
-    'vue/one-component-per-file': 'off',
-    'vue/custom-event-name-casing': 'off',
-    'vue/v-on-event-hyphenation': 'off',
-  }
+  // rules: {
+  //   "@typescript-eslint/explicit-function-return-type": "off",
+  // }
 };

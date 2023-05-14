7b068b1291171c4baac4288df6bbc61576720ba4
CCherry07 <2405693142@qq.com>
2023-05-15 01:12:17 +0800

    ci: update eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,28 @@ module.exports = {
     sourceType: 'module',
   },
   rules: {
-    "@typescript-eslint/explicit-function-return-type": "off",
-  }
+    "@typescript-eslint/explicit-function-return-type": 0,
+
+  },
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint'],
+      extends: ['plugin:@typescript-eslint/recommended'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 0,
+        '@typescript-eslint/ban-types': 0,
+        '@typescript-eslint/consistent-type-imports': 'error',
+        '@typescript-eslint/explicit-module-boundary-types': 0,
+        '@typescript-eslint/no-empty-function': 0,
+        '@typescript-eslint/no-non-null-assertion': 0,
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { vars: 'all', args: 'after-used', ignoreRestSiblings: true },
+        ],
+        '@typescript-eslint/ban-ts-comment': 0,
+      }
+    },
+  ]
 };
